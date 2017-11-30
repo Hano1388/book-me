026a4fa5725209e17bcec787b226505407eec5fb
Hano <hinsul3113@gmail.com>
2017-11-29 16:41:08 -0800

    created user schema and add functionality to create a user in database

---
+++ models/user.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const bcrypt = require('bcryptjs');  // to hash our password
+
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    index: true
+  },
+  password: {
+    type: String
+  },
+  email: {
+    type: String
+  },
+  name: {
+    type: String
+  }
+});
+
+var User = module.exports = mongoose.model('User', UserSchema);
+
+module.exports.createUser = (newUser, callback) => {
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(newUser.password, salt, (err, hash) => {
+      newUser.password = hash;
+      newUser.save(callback);
+    });
+  });
+}
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const User = require('../models/user');
+
 // Register
 router.get('/register', (req, res) => {
   res.render('register');
@@ -33,7 +35,18 @@ router.post('/register', (req, res) => {
       errors: errors
     });
   } else {
-    console.log('passed');
+    var newUser = new User({
+      name: name,
+      email: email,
+      username: username,
+      password: password
+    });
+    User.createUser(newUser, (err, user) => {
+      if(err) throw err;
+      console.log(user);
+    });
+    req.flash('success_msg', 'You are registered and can now login');
+    res.redirect('/users/login');
   }
 });
 
