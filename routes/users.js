3fadc0efee8f55f23bdcf70966038baa21fa11d1
Hano <hinsul3113@gmail.com>
2017-11-29 16:03:05 -0800

    settup register user form validations

--- routes/users.js
+++ routes/users.js
@@ -11,4 +11,30 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
+// Register user
+router.post('/register', (req, res) => {
+  let name = req.body.name;
+  let email = req.body.email;
+  let username = req.body.username;
+  let password = req.body.password;
+  let password2 = req.body.password2;
+
+  // validations
+  req.checkBody('name', 'Name is required').notEmpty();
+  req.checkBody('email', 'Email is required').notEmpty();
+  req.checkBody('email', 'Email is not valid').isEmail();
+  req.checkBody('username', 'Username is required').notEmpty();
+  req.checkBody('password', 'Password is required').notEmpty();
+  req.checkBody('password2', 'Passwords do not much').equals(req.body.password);
+
+  let errors = req.validationErrors();
+  if (errors) {
+    res.render('register', {
+      errors: errors
+    });
+  } else {
+    console.log('passed');
+  }
+});
+
 module.exports = router;
